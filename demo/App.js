2c8a3fec752f7de2e093ead7f290bada8caf7faa
Stephen Van Gordon <stephen.van.gordon@gmail.com>
2017-03-25 13:15:33 -0600

    move inline styles to css

--- demo/App.js
+++ demo/App.js
@@ -144,7 +144,7 @@ class App extends Component {
 						ranks={this.state.ranks} files={this.state.files}
 						pieceDefinitions={this.gamePresets[this.state.gameType].pieceDefinitions}
 					/>
-				<p style={{display: 'inline-block', position: 'absolute'}}><strong>{this.state.lastMessage}</strong></p>
+				<p className={"lastMessage"}><strong>{this.state.lastMessage}</strong></p>
 			</div>
     );
   }
